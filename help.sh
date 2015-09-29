c6cf9255617c3d3eb4f9067c39cb0e7ca326dff9
Sergey Bondarenko <broken@propeople.com.ua>
2015-09-29 05:24:02 +0400

    Improved help messages

--- help.sh
+++ help.sh
@@ -1,58 +1,64 @@
 #!/usr/bin/env bash
 
-_doc_param()
+_clib_green()
 {
-    coloredMessage "${1}" 2
+  coloredMessage "${1}" 2
 }
 
-_doc_yellow()
+_clib_yellow()
 {
-    coloredMessage "${1}" 3
+  coloredMessage "${1}" 3
 }
 
-_doc_blue()
+_clib_blue()
 {
-    coloredMessage "${1}" 4
+  coloredMessage "${1}" 4
+}
+
+_clib_command()
+{
+  local arguments=""
+
+  for ((i = 4; i <= $#; i++)); do
+    arguments+=" `_clib_green "<${!i}>"`"
+  done
+
+  echo "`_clib_yellow "Command:"`
+  `_clib_blue "${1}"`${arguments}
+
+  `_clib_yellow "Description:"`${2}
+  `_clib_yellow "Examples:"`${3}"
 }
 
 _help_clone()
 {
-  echo "`_doc_yellow "Command:"`
-  `_doc_blue "clone"` `_doc_param "<host>"` `_doc_param "<project>"` `_doc_param "<git clone parameters>"`
-
-  `_doc_yellow "Description:"`
-  `_doc_blue "Clone Git repository from remote."`
-  `_doc_blue "- all"` `_doc_param "git clone"` `_doc_blue "arguments available"`
-  `_doc_blue "- any host can be added/overriden in file, where you include the CLIB"`
-
-  `_doc_yellow "Examples:"`
-  `_doc_blue "clone drupal drupal --branch=7.x"`
-  `_doc_blue "clone ffw sla/unga test"`
-  `_doc_blue "clone gh br0ken-/tqextension --quiet"`"
+  _clib_command clone "
+  Clone Git repository from remote.
+  - all `_clib_green "git clone"` arguments available
+  - any host can be added/overriden in file, where you include the CLIB
+  " "
+  clone drupal drupal --branch=7.x
+  clone ffw sla/unga test
+  clone gh br0ken-/tqextension --quiet
+  " "host" "project" "git clone parameters"
 }
 
 _help_jira()
 {
-  echo "`_doc_yellow "Command:"`
-  `_doc_blue "jira"` `_doc_param "<project>"`
-
-  `_doc_yellow "Description:"`
-  `_doc_blue "Open the JIRA project or task in default browser. Project key is insensitive."`
-
-  `_doc_yellow "Examples:"`
-  `_doc_blue "jira pva"`
-  `_doc_blue "jira pva-177"`"
+  _clib_command jira "
+  Open JIRA project or task in default browser. Project key is insensitive.
+  " "
+  jira pva
+  jira pva-177
+  " "project"
 }
 
 _help_g()
 {
-  echo "`_doc_yellow "Command:"`
-  `_doc_blue "g"` `_doc_param "<query>"`
-
-  `_doc_yellow "Description:"`
-  `_doc_blue "Perform search in Google. All search syntax available."`
-
-  `_doc_yellow "Examples:"`
-  `_doc_blue "g rammstein"`
-  `_doc_blue "g latin site: wikipedia.org"`"
+  _clib_command g "
+  Perform search in Google. All search syntax available.
+  " "
+  g rammstein
+  g latin site: wikipedia.org
+  " "query"
 }
